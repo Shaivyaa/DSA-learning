ab3ecd5a02487c6cafba3b7b630d550d290b4572
Shaivyaa <shukla.shaivya19991@gmail.com>
2026-03-03 00:43:02 +0530

    linked list

---
+++ LinkedList/DesignALinkedList.js
@@ -0,0 +1,21 @@
+//
+
+// Representation of a node
+
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+// Create a new node
+
+let newNode = new Node(5);
+
+//
+
+// Representation of Linked List
+
+function MyLinkedList() {
+  this.head = null;
+  this.size = 0;
+}
